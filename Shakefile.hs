51da1038b008ae77e80f39c85dd0d4fa452767cd
Yota Toyama <raviqqe@gmail.com>
2016-12-13 09:55:21 +0900

    Add ping test

--- Shakefile.hs
+++ Shakefile.hs
@@ -3,5 +3,10 @@ import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
 
+
+hostsFile = "hosts"
+
+
 main :: IO ()
 main = shakeArgs shakeOptions $ do
+  "ping" ~> cmd "ansible all -i" hostsFile "-m ping"
